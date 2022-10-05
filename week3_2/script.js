a9bd0f8e4a047c7f6dda59285b6604dc9a2ea609
zeroshic <zeroshic@gmail.com>
2022-10-05 14:32:11 -0700

    Button action

---
+++ week3_2/script.js
@@ -0,0 +1,45 @@
+element {
+}
+.sc-watch .sc-watch-compare__list-figure {
+  padding: 70px 0 60px;
+  padding: 4.86111vw 0 4.16667vw;
+}
+.sc-watch * {
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  vertical-align: top;
+}
+body, button, dd, div, dl, dt, fieldset, form, h1, h2, h3, h4, h5, h6, iframe, li, ol, p, td, th, ul {
+  margin: 0;
+  padding: 0;
+}
+li {
+  list-style: none;
+}
+.sc-watch .swiper-container {
+  list-style: none;
+}
+#contents.sc-watch {
+  font-family: SamsungOne400;
+}
+.sc-watch {
+  color: #000;
+}
+.l_body {
+  color: #000;
+}
+body {
+  font-family: sssM,sans-serif;
+  font-size: 12px;
+  line-height: 1.3;
+  color: #4b4c4c;
+  -webkit-text-size-adjust: none;
+}
+element {
+  --vh: 797px;
+  --scrollbarWidth: 17px;
+  --outer-vh: 797px;
+  --snbh: 96px;
+}
